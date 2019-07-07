ea856f5aac16be3bcc2d086bfae3b6fd223eb47b
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-07 21:21:17 +0000

    fixing scraper class methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,23 +6,36 @@ require "pry"
 
 
 class CLI 
-  attr_accessor :link, :scopeslibrary 
-  @@all = []
-  
-  def initialize(link = "https://www.horoscope.com/us/index.aspx")
-    @link = link 
-    new = Scraper.new(link)
-    @scopeslibrary = new.create_from_collection
-    @@all << self 
-  end 
-  
+
   def run
+    
     puts "CLI is running"
   end 
   
-  def self.make_signs
+  def make_signs
     signs_array = Scraper.scrape_index_page
     ZodiacSign.create_from_collection(signs_array)
+  end
+  
+  def add_attributes_to_signs
+    ZodiacSign.all.each do |sign|
+      attributes = Scraper.scrape_info(sign.url)
+      Zodiac.add_attributes(attributes)
+    end
+  end
+
+  def display_students
+    Student.all.each do |student|
+      puts "#{student.name.upcase}".colorize(:blue)
+      puts "  location:".colorize(:light_blue) + " #{student.location}"
+      puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
+      puts "  bio:".colorize(:light_blue) + " #{student.bio}"
+      puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
+      puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
+      puts "  github:".colorize(:light_blue) + " #{student.github}"
+      puts "  blog:".colorize(:light_blue) + " #{student.blog}"
+      puts "----------------------".colorize(:green)
+    end
   end
   
 end 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,7 @@ class Scraper
       }
     end
     signs_list
+    binding.pry
   end
   
   
@@ -28,31 +29,37 @@ class Scraper
   url = sign.url
   html = Nokogiri.HTML(open(url))
   
-   sign.today_date = html.css("div.grid.grid-right-sidebar div p strong.date").text
+  sign_atts = {}
+  
+   sign_atts[:today_date] = html.css("div.grid.grid-right-sidebar div p strong.date").text
    
       horoscopeAllText = html.css("div.grid.grid-right-sidebar div p").text
       allTextArray = horoscopeAllText.split(" - ")
       horoscope_text = allTextArray[1]
       horoscope_split = horoscope_text.split("Confused about")
       horoscope_only = horoscope_split[0]
-   sign.horoscope = horoscope_only
+   sign_atts[:horoscope] = horoscope_only
    
-       link_info = html.css("div.grid.grid-right-sidebar div.more-btns.more-horoscopes a#src-horo-btn-love").attribute('href').value
-   sign.love_link = "https://www.horoscope.com" + link_info
+       sign_atts[:link_info] = html.css("div.grid.grid-right-sidebar div.more-btns.more-horoscopes a#src-horo-btn-love").attribute('href').value
+   sign_atts[:love_link] = "https://www.horoscope.com" + link_info
+   
+   sign_atts
   end
   
   def self.scrape_love_info(sign)
     url = sign.love_link
     html = Nokogiri.HTML(open(url))
+   sign_att = {}
    
       loveFullText = html.css("div.grid.grid-right-sidebar p").text
       loveArray1 = loveFullText.split(" - ")
       horo = loveArray1[1]
       horoSplit = horo.split("Meet highly")
-   sign.love_scope = horoSplit[0]
+   sign_att[:love_scope] = horoSplit[0]
+   sign_att
   end 
     
 end
 
-# Scraper.scrape_index_page
-# Scraper.scrape_love_info("https://www.horoscope.com/us/horoscopes/love/horoscope-love-daily-today.aspx?sign=1")
+Scraper.scrape_index_page
+#Scraper.scrape_love_info("https://www.horoscope.com/us/horoscopes/love/horoscope-love-daily-today.aspx?sign=1")
